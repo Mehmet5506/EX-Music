794ef06e1f4549ca57e53aeb5c1bdea8c19538ab
Talia Turkish Music <83137081+Mehmet5506@users.noreply.github.com>
2021-08-24 00:26:23 +0300

    Update broadcast.py

--- EXmusic/modules/broadcast.py
+++ EXmusic/modules/broadcast.py
@@ -17,17 +17,17 @@ async def broadcast(_, message: Message):
     if message.from_user.id not in SUDO_USERS:
         return
     else:
-        wtf = await message.reply("`starting broadcast...`")
+        wtf = await message.reply("`yayını başlatma...`")
         if not message.reply_to_message:
-            await wtf.edit("please reply to a message to start broadcast!")
+            await wtf.edit("lütfen yayını başlatmak için bir iletiyi yanıtlayın!")
             return
         lmao = message.reply_to_message.text
         async for dialog in exmusic.iter_dialogs():
             try:
                 await exmusic.send_message(dialog.chat.id, lmao)
                 sent = sent+1
-                await wtf.edit(f"`broadcasting...` \n\n**sent to:** `{sent}` chats \n**failed in:** {failed} chats")
+                await wtf.edit(f"`Yayın...` \n\n**gönderildiği yer:** `{sent}` Sohbet \n**başarısız oldu:** {failed} chats")
                 await asyncio.sleep(3)
             except:
                 failed=failed+1
-        await message.reply_text(f"`gcast succesfully` \n\n**sent to:** `{sent}` chats \n**failed in:** {failed} chats")
+        await message.reply_text(f"`gcast başarıyla` \n\n**gönderildiği yer:** `{sent}` sohbet \n**başarısız oldu:** {failed} chats")
