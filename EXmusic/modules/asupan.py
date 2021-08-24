26f8aaf7c41e0ff6ee5c770670b8f6c9216499cc
Talia Turkish Music <83137081+Mehmet5506@users.noreply.github.com>
2021-08-24 23:02:04 +0300

    Update asupan.py

--- EXmusic/modules/asupan.py
+++ EXmusic/modules/asupan.py
@@ -7,31 +7,31 @@ from pyrogram import Client
 from EXmusic.config import BOT_USERNAME
 from EXmusic.helpers.filters import command
 
-@Client.on_message(command(["asupan", f"asupan@{BOT_USERNAME}"]))
+@Client.on_message(command(["asupan", f"asupan@Efsanestar_bot"]))
 async def asupan(client, message):
     try:
         resp = requests.get("https://tede-api.herokuapp.com/api/asupan/ptl").json()
         results = f"{resp['url']}"
         return await client.send_video(message.chat.id, video=results)
     except Exception:
-        await message.reply_text("`Something went wrong LOL...`")
+        await message.reply_text("`Bir şeyler ters gitti...`")
 
 
-@Client.on_message(command(["wibu", f"wibu@{BOT_USERNAME}"]))
+@Client.on_message(command(["wibu", f"wibu@Efsanestar_bot"]))
 async def wibu(client, message):
     try:
         resp = requests.get("https://tede-api.herokuapp.com/api/asupan/wibu").json()
         results = f"{resp['url']}"
         return await client.send_video(message.chat.id, video=results)
     except Exception:
-        await message.reply_text("`Something went wrong LOL...`")
+        await message.reply_text("`Bir şeyler ters gitti...`")
 
 
-@Client.on_message(command(["chika", f"chika@{BOT_USERNAME}"]))
+@Client.on_message(command(["chika", f"chika@Efsanestar_bot"]))
 async def chika(client, message):
     try:
         resp = requests.get("https://tede-api.herokuapp.com/api/chika").json()
         results = f"{resp['url']}"
         return await client.send_video(message.chat.id, video=results)
     except Exception:
-        await message.reply_text("`Something went wrong LOL...`")
+        await message.reply_text("`Bir şeyler ters gitti....`")
